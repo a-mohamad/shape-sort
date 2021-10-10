ec9c2d3149296ef57a9ef83e933c9d08ca996f31
Amir Mohamad <amohamad@my.yorku.ca>
2021-10-09 23:50:45 -0400

    Added create() to create random shape

--- src/main/java/shapesort/model/ShapeFactory.java
+++ src/main/java/shapesort/model/ShapeFactory.java
@@ -1,6 +1,9 @@
 package shapesort.model;
 
+import shapesort.util.ColorUtility;
+
 import java.awt.*;
+import java.util.Random;
 
 public final class ShapeFactory {
     private static ShapeFactory shapeFactory;
@@ -23,7 +26,7 @@ public final class ShapeFactory {
         };
     }
 
-    public Shape createAt(ShapeType shapeType, Point point) {
+    public Shape create(ShapeType shapeType, Point point) {
         return switch (shapeType) {
             case RECTANGLE -> new Rectangle(point);
             case SQUARE -> new Square(point);
@@ -32,12 +35,31 @@ public final class ShapeFactory {
         };
     }
 
-    public Shape createAt(ShapeType shapeType, int x, int y) {
+    public Shape create(ShapeType shapeType, int x, int y) {
         return switch (shapeType) {
             case RECTANGLE -> new Rectangle(new Point(x, y));
             case SQUARE -> new Square();
             case CIRCLE -> new Circle(new Point(x, y));
             default -> throw new IllegalArgumentException("Not a valid shape.");
         };
+    }
+
+    public Shape create() {
+        Random rand = new Random();
+        int MAX_SIZE = 1000;
+        int shapeIndex = rand.nextInt(ShapeType.values().length);
+        ShapeType shapeType = ShapeType.values()[shapeIndex];
+        Shape shape = shapeFactory.create(shapeType);
+
+        int width = rand.nextInt(MAX_SIZE);
+        int height = rand.nextInt(MAX_SIZE);
+
+        shape.setColor(ColorUtility.getRandomColor());
+        shape.setBoundingDimension(switch (shapeType) {
+            case RECTANGLE -> new Dimension(width, height);
+            case SQUARE, CIRCLE -> new Dimension(width, width);
+            default -> throw new IllegalArgumentException("Not a valid shape.");
+        });
+        return shape;
     }
 }
